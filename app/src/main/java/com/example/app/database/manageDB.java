c5bb4f49b86a8cbff8976ab592495595c8ab78f8
Aitazaz gilani (shg374) <shg374@usask.ca>
2022-10-19 14:30:09 -0600

    added sqlite lib support, manageDB class

---
+++ app/src/main/java/com/example/app/database/manageDB.java
@@ -0,0 +1,12 @@
+package com.example.app.database;
+import java.sql.*;
+import org.sqlite.JDBC;
+
+public class manageDB {
+
+    private Connection LoginDB;
+
+    private Connection UserDB;
+
+
+}
