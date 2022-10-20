1c657ded5887056a394b096b08c4547e5f81385d
ThomasFeltham <thf441@usask.ca>
2022-10-20 13:51:56 -0600

    Created Controller folder and the AppController class. Added some empty skeleton methods to AppController.java based on the UML diagram.

---
+++ app/src/Controller/AppController.java
@@ -0,0 +1,31 @@
+public class AppController{
+
+    public Task createNewTask(){
+
+    }
+
+    public Client createNewClient(){
+
+    }
+
+    public User createNewUser(){
+
+    }
+
+    public void deleteTask(){
+
+    }
+
+    public void updateTask(){
+
+    }
+
+    public void updateContact(){
+
+    }
+
+    public void deleteContact(){
+        
+    }
+
+    }
